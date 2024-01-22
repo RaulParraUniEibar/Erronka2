01a29a58717540703679c146bfdd53335b94c26c
Haritz Otero <96473981+HaritzOtero@users.noreply.github.com>
2024-01-22 11:59:37 +0100

    Proyecto iniciado

---
+++ didaktikApp/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'didaktikApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
