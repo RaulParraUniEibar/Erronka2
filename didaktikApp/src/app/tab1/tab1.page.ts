2e799b49dda7835293234001f03f134377ee08d1
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2024-01-24 11:00:49 +0100

    Tab1 Audioak

--- didaktikApp/src/app/tab1/tab1.page.ts
+++ didaktikApp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonButton } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,44 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  isAudioPlaying: boolean = false;
+
+
+  @ViewChild('btnLehenengoDesberdintasuna', { static: false }) btnLehenengoDesberdintasuna: IonButton | undefined;
+
+  playAudio(audioId: string) {
+    if (this.isAudioPlaying) {
+      // Si ya hay un audio reproduciéndose, no hagas nada
+      return;
+    }
+
+    const audio = document.getElementById(audioId) as HTMLAudioElement;
+
+    audio.onended = () => {
+      // Se llama cuando la reproducción del audio termina
+      this.isAudioPlaying = false;
+    };
+
+    this.isAudioPlaying = true;
+
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+      audio.currentTime = 0;
+      this.isAudioPlaying = false;
+    }
+  }
+
+  // ... Otro código ...
+
+
   constructor() {}
 
+  // Asegúrate de llamar a esta función después de que la vista haya sido inicializada
+  ngAfterViewInit() {
+    console.log('Button reference:', this.btnLehenengoDesberdintasuna);
+  }
+
+  
 }
