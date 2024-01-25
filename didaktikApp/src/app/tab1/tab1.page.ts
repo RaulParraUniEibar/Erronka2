8f46d983be81f0066426faaf8b0b8e8cb5612c60
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2024-01-25 10:37:26 +0100

    hurrengoa btn + stopAudio

--- didaktikApp/src/app/tab1/tab1.page.ts
+++ didaktikApp/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonButton } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -9,7 +10,6 @@ import { IonButton } from '@ionic/angular';
 export class Tab1Page {
 
   isAudioPlaying: boolean = false;
-
 
   @ViewChild('btnLehenengoDesberdintasuna', { static: false }) btnLehenengoDesberdintasuna: IonButton | undefined;
 
@@ -37,15 +37,31 @@ export class Tab1Page {
     }
   }
 
-  // ... Otro código ...
+  stopAllAudio() {
+    // Detener la reproducción de todos los elementos de audio
+    for (let i = 1; i <= 4; i++) {
+      const audio = document.getElementById("audio" + i) as HTMLAudioElement;
+      if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    }
+    this.isAudioPlaying = false;
+  }// ... Otro código ...
 
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
 
   // Asegúrate de llamar a esta función después de que la vista haya sido inicializada
   ngAfterViewInit() {
     console.log('Button reference:', this.btnLehenengoDesberdintasuna);
   }
-
+  hurrengoaButtonClicked() {
+    this.stopAllAudio();
+    
+    this.navCtrl.navigateForward('/tabs/tab2'); // Ajusta la ruta según tu configuración de enrutamiento
+  
+  }
+  
   
 }
