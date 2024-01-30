f05f1f2e446b5561f253780a9b8614e2b01a5e46
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2024-01-30 08:55:12 +0100

    Hurrengoa btn disabled
    Poner el boton en disabled y si hay audio reproduciendose detenerlo.

--- didaktikApp/src/app/tab2/tab2.page.ts
+++ didaktikApp/src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonButton } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
@@ -11,32 +11,36 @@ import { Router } from '@angular/router';
 export class Tab2Page {
   paragraphVisible = false;
   showButton: boolean = true;
-  
-  mostrarParrafo() {
-    this.paragraphVisible = !this.paragraphVisible;
-    this.showButton = false;
-  }
-  
+
+  audioSanJuanArkuaPlayed: boolean = false;
+  audioAriketaPlayed: boolean = false;
+
   isAudioPlaying: boolean = false;
-  
+
   @ViewChild('btnLehenengoDesberdintasuna', { static: false }) btnLehenengoDesberdintasuna: IonButton | undefined;
 
   playAudio(audioId: string) {
     if (this.isAudioPlaying) {
-      // Si ya hay un audio reproduciéndose, no hagas nada
+      // Si ya hay un audio reproduciéndose, deténlo
+      this.stopAllAudio();
       return;
     }
-  
+
     const audio = document.getElementById(audioId) as HTMLAudioElement;
-  
+
     audio.onended = () => {
       // Se llama cuando la reproducción del audio termina
       this.isAudioPlaying = false;
-  
+
+      if (audioId === 'SanJuanArkua') {
+        this.audioSanJuanArkuaPlayed = true;
+      } else if (audioId === 'Ariketa') {
+        this.audioAriketaPlayed = true;
+      }
     };
-  
+
     this.isAudioPlaying = true;
-  
+
     if (audio.paused) {
       audio.play();
     } else {
@@ -45,28 +49,34 @@ export class Tab2Page {
       this.isAudioPlaying = false;
     }
   }
-  constructor(private router: Router,private navCtrl: NavController) { }
-  
+
+  stopAllAudio() {
+    // Detener todos los audios
+    const allAudios = document.querySelectorAll('audio') as NodeListOf<HTMLAudioElement>;
+    allAudios.forEach(audio => {
+      audio.pause();
+      audio.currentTime = 0;
+    });
+
+    this.isAudioPlaying = false;
+  }
+
+  mostrarParrafo() {
+    this.paragraphVisible = !this.paragraphVisible;
+    this.showButton = false;
+  }
+
+  constructor(private router: Router, private navCtrl: NavController) { }
+
   redirigirAKahoot() {
+    // Detener todos los audios antes de redirigir a Kahoot
+    this.stopAllAudio();
     window.open('https://create.kahoot.it/share/san-juan-arkua/46a3c0ae-a6fe-4a86-9a6d-8a3f0892912d', '_blank');
   }
-  
-  stopAllAudio() {
-    // Detener la reproducción de todos los elementos de audio
-    for (let i = 1; i <= 4; i++) {
-      const audio = document.getElementById("audio" + i) as HTMLAudioElement;
-      if (audio) {
-        audio.pause();
-        audio.currentTime = 0;
-      }
-    }
-    this.isAudioPlaying = false;
-  }// ... Otro código ...
 
   hurrengoaButtonClicked() {
+    // Detener todos los audios antes de navegar a la siguiente página
     this.stopAllAudio();
-    
     this.navCtrl.navigateForward('/tabs/tab3'); // Ajusta la ruta según tu configuración de enrutamiento
-  
   }
 }
