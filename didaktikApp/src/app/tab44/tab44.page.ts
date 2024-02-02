36d0a1541081567ceffe5d736d1afad049a451c2
Haritz Otero <96473981+HaritzOtero@users.noreply.github.com>
2024-02-02 10:24:41 +0100

    Historia acabada

--- didaktikApp/src/app/tab44/tab44.page.ts
+++ didaktikApp/src/app/tab44/tab44.page.ts
@@ -8,39 +8,68 @@ import { NavController } from '@ionic/angular';
 export class Tab44Page  {
 
 
+  ngOnInit() {
+    const speakerElement = document.querySelector('.speaker');
+    let index = 0;
+    if (speakerElement) {
+      speakerElement.textContent = 'Pertak Pirata 💬';
+    }
+  }
+
+  revealText() {
+    const dialogElement = document.getElementById('dialog69');
+    const textToReveal = 'Hala ere, hiru naufragok flotagailu inprobisatu bat egin zuten. Tamalez, bakarra geratu zen flotagailuan bizirik.';
+    let index = 0;
+  
+    const reveal = () => {
+      if (dialogElement) {
+        dialogElement.textContent = textToReveal.slice(0, index);
+        index++;
+  
+        if (index <= textToReveal.length) {
+          setTimeout(reveal, 100); // Ajusta el tiempo para controlar la velocidad
+        }
+      }
+    };
+  
+    reveal();
+  }
+  
   isAudioPlaying: boolean = false;
   LehenengoAudioaPlayed: boolean = false;
-
+  
   constructor(private navCtrl : NavController) {}
-
+  
   playAudio(audioId: string) {
     if (this.isAudioPlaying) {
       // Si ya hay un audio reproduciéndose, deténlo
       //this.stopAllAudio();
       return;
     }
-
+  
     const audio = document.getElementById(audioId) as HTMLAudioElement;
-
+  
     audio.onended = () => {
       // Se llama cuando la reproducción del audio termina
       this.isAudioPlaying = false;
-
+  
       if (audioId === '4Audio') {
         this.LehenengoAudioaPlayed = true;
       }
     };
-
+  
     this.isAudioPlaying = true;
-
+  
     if (audio.paused) {
       audio.play();
+      this.revealText(); // Llamar a revealText después de iniciar la reproducción del audio
     } else {
       audio.pause();
       audio.currentTime = 0;
       this.isAudioPlaying = false;
     }
   }
+
 
   stopAllAudio() {
     // Detener todos los audios
--- didaktikApp/src/app/tab45/tab45.page.ts
+++ didaktikApp/src/app/tab45/tab45.page.ts
@@ -9,33 +9,61 @@ import { NavController } from '@ionic/angular';
 export class Tab45Page {
 
 
+  ngOnInit() {
+    const speakerElement = document.querySelector('.speaker');
+    let index = 0;
+    if (speakerElement) {
+      speakerElement.textContent = 'Pirri 💬';
+    }
+  }
+
+  revealText() {
+    const dialogElement = document.getElementById('dialog10');
+    const textToReveal = 'Guztira, 143 pertsonak hil ziren. Marinelen omenez, Bermeon, arrantzale museoaren ondoan estatua hauek aurki ditzakegu. Estatuek marinel horien familia, emazteen eta lagunen itxaropena eta  tristura sinbolizatzen dute.';
+    let index = 0;
+  
+    const reveal = () => {
+      if (dialogElement) {
+        dialogElement.textContent = textToReveal.slice(0, index);
+        index++;
+  
+        if (index <= textToReveal.length) {
+          setTimeout(reveal, 75); // Ajusta el tiempo para controlar la velocidad
+        }
+      }
+    };
+  
+    reveal();
+  }
+  
   isAudioPlaying: boolean = false;
   LehenengoAudioaPlayed: boolean = false;
-
+  
   constructor(private navCtrl : NavController) {}
-
+  
   playAudio(audioId: string) {
     if (this.isAudioPlaying) {
       // Si ya hay un audio reproduciéndose, deténlo
       //this.stopAllAudio();
       return;
     }
-
+  
     const audio = document.getElementById(audioId) as HTMLAudioElement;
-
+  
     audio.onended = () => {
       // Se llama cuando la reproducción del audio termina
       this.isAudioPlaying = false;
-
+  
       if (audioId === '5Audio') {
         this.LehenengoAudioaPlayed = true;
       }
     };
-
+  
     this.isAudioPlaying = true;
-
+  
     if (audio.paused) {
       audio.play();
+      this.revealText(); // Llamar a revealText después de iniciar la reproducción del audio
     } else {
       audio.pause();
       audio.currentTime = 0;
